b1ad9eaf9dc79b2c5f16bebc091fe402ec4768f6
killsystem10 <killsystem10@outlook.com>
2023-09-04 16:28:54 +0900

    Change Boram_Programming

--- Boram_Programming/Boram_Programming.cpp
+++ Boram_Programming/Boram_Programming.cpp
@@ -1,17 +1,11 @@
-
 #include <stdio.h>
 int main() {
-	int a;
-	printf("=====거짓말 판별기======\n");
-	printf("박지훈은 씹덕인가? 1.맞다 2.아니다\n");
+	int a, b;
 	scanf("%d", &a);
-	if (a == 1) {
-		printf("진실이다");
-	}
-	else if (a==2) {
-		printf("너는 박지훈이 씹덕이란 것을 부정하고 있다. 거짓이다");
-	}
-	else {
-		printf("이상한 것을 입력하는 새끼는 꺼져라");
+	reget:
+	scanf("%d", &b);
+	if (a-- != 0) {
+		printf("%d\n", b);
+		goto reget;
 	}
 }
